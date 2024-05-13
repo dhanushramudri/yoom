98e0ff6a073d4af4944b43810a8c1fb7f59b424b
dhanushramudri <ramudridhanush@gmail.com>
2024-05-13 23:04:19 +0530

    first commit

---
+++ app/(root)/(home)/meeting/[id]/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const Meeting = ({ params }: { params: { id: string } }) => {
+  return <div>Meeting Room : {params.id}</div>;
+};
+
+export default Meeting;
