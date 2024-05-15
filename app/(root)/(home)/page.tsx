edfe95094c4052253c111107ac6e8773a87e7d35
Dhanush Ramudri <114290461+dhanushramudri@users.noreply.github.com>
2024-05-15 22:28:50 +0530

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <section className="flex size-full flex-col text-white ">
-      <h1 className="text-3xl font-bold ">Home</h1>
+      <h1 className="text-3xl font-bold ">Home , watched js,angela course </h1>
     </section>
   );
 };
