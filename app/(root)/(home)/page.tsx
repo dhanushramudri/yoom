e4204d8ca2a243cc2101daf433ea7bc76e44dac7
dhanushramudri <ramudridhanush@gmail.com>
2024-05-17 23:45:27 +0530

    new commit

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const Home = () => {
   return (
     <section className="flex size-full flex-col text-white ">
-      <h1 className="text-3xl font-bold ">Home , watched js, scrapping(playwright) </h1>
+      <h1 className="text-3xl font-bold ">
+        Home , watched js, scrapping(playwright){" "}
+      </h1>
     </section>
   );
 };
